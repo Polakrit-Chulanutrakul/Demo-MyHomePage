7e68303d9010bdaba7805cbc2201c28dd2736c60
Polakrit Chulanutrakul <141233391+Polakrit-Chulanutrakul@users.noreply.github.com>
2023-08-02 21:42:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    Particles.init({
+        selector: '.background',
+        maxParticles: 25,
+        connectParticles: 'true',
+        speed: 1,
+        minDistance: 140,
+        sizeVariations: 8,
+        color: '#87c3fa',
+    })
+}
